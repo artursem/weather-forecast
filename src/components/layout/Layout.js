b64c4cfaa099e89ac93129c4eb95a4b0a3efefd9
artursem <artur.semeniuk@gmail.com>
2022-01-14 21:19:26 +0100

    add background blur

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,6 +1,9 @@
 const Layout = ({ bg, children }) => {
 	const weatherBg = {
-		1: { color: 'bg-bg-100', image: 'bg-main-background' },
+		1: {
+			color: 'bg-main-background',
+			image: 'bg-main-background',
+		},
 		2: { color: 'bg-bg-200', image: 'bg-sun-cloud-background' },
 		3: { color: 'bg-bg-300', image: 'bg-cloud-background' },
 		4: { color: 'bg-bg-300', image: 'bg-cloud-background' },
@@ -10,18 +13,21 @@ const Layout = ({ bg, children }) => {
 		13: { color: 'bg-bg-600', image: 'bg-snow-background' },
 		50: { color: 'bg-bg-700', image: 'bg-fog-background' },
 	};
-	console.log(weatherBg[10].color);
-	console.log(weatherBg[10].image);
 	return (
 		<main
-			className={`flex flex-col justify-start items-center w-full min-h-screen
-			  ${weatherBg[bg].color}`}
+			className={`w-full min-h-screen
+			  bg-cover ${weatherBg[bg].image}`}
 		>
 			<div
-				className={`flex flex-col items-stretch w-full max-w-2xl min-h-screen
-			 bg-cover shadow-2xl ${weatherBg[bg].image}`}
+				className='flex flex-col justify-start items-center 
+			w-full min-h-screen backdrop-filter backdrop-blur-xl'
 			>
-				{children}
+				<div
+					className={`flex flex-col items-stretch w-full max-w-3xl min-h-screen
+			 bg-cover shadow-2xl ${weatherBg[bg].image}`}
+				>
+					{children}
+				</div>
 			</div>
 		</main>
 	);
